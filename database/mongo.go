2a96cdc83ce3bfcb07ce028ff50c61a21dfde5ba
Henrique Sulimann <henrique.sulimann@gmail.com>
2022-05-20 12:46:02 -0300

    create login and users endpoints with jwt authentication

--- database/mongo.go
+++ database/mongo.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -11,8 +12,7 @@ import (
 )
 
 func StartMongo() *mongo.Client {
-	// client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://" + os.Getenv("DATABASE_USER") + ":" + os.Getenv("DATABASE_PASS") + "@" + os.Getenv("DATABASE_SERVER") + ":" + os.Getenv("DATABASE_PORT")))
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:123456@localhost:27023"))
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://" + os.Getenv("DATABASE_USER") + ":" + os.Getenv("DATABASE_PASS") + "@" + os.Getenv("DATABASE_SERVER") + ":" + os.Getenv("DATABASE_PORT")))
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	err = client.Connect(ctx)
